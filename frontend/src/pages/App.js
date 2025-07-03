92950eea804f10b5ff1886f0e5293c3b16300208
anaiysaa <adeswal@purdue.edu>
2025-07-03 13:53:46 -0700

    top bar draft

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,8 +1,14 @@
 import '../css/App.css';
+import TopBar from '../componenets/TopBar';
+import TopBackground from '../assets/TopBackground.jpg';  //change immage asset link if needed
+
 function App() {
   return (
-    <div>
-      <h1>Test page Yippee</h1>
+    <div className="app">
+      <div className="top-background">
+        <img src={TopBackground} alt="Top Background" />
+      </div>
+      <TopBar />
     </div>
   );
 }
