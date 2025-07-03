60a6b631b2037578595cc180d03411e42b8214ad
Samved Mamillapalli <71089700+samvedmamillapalli@users.noreply.github.com>
2025-07-03 13:20:52 -0700

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome To the Portal</h1>
+      {/*figma stuff goes here*/}
+    </div>
+  );
+}
+
+export default Home;
